b1fa8ee2b6649e4709b08dba5a281eba82c6c384
MickYuan <384077767@qq.com>
2019-09-29 21:57:13 +0800

    Add files via upload

---
+++ src/main/java/customization/hyren/type/Hbigdecimal.java
@@ -0,0 +1,38 @@
+package customization.hyren.type;
+
+import com.google.common.collect.ImmutableList;
+import com.ngdata.hbaseindexer.parse.ByteArrayValueMapper;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.util.Bytes;
+
+import java.math.BigDecimal;
+import java.util.Collection;
+
+public class Hbigdecimal implements ByteArrayValueMapper {
+
+    private static Log log = LogFactory.getLog(Hbigdecimal.class);
+    /**
+     * Map a byte array to a collection of values. The returned collection can be empty.
+     * <p>
+     * If a value cannot be mapped as requested, it should log the error and return an empty collection.
+     *
+     * @param input byte array to be mapped
+     * @return mapped values
+     */
+    @Override
+    public Collection<? extends Object> map(byte[] input) {
+        try {
+            return ImmutableList.of(mapInternal(Bytes.toString(input)));
+        } catch (IllegalArgumentException e) {
+            log.warn(
+                    String.format("Error mapping byte value %s to %s", Bytes.toStringBinary(input),
+                            BigDecimal.class.getName()), e);
+            return ImmutableList.of();
+        }
+    }
+
+    private BigDecimal mapInternal(String toString) {
+        return BigDecimal.valueOf(Long.parseLong(toString));
+    }
+}
